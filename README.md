3e1eebe975a17eacd203bef371efb59ded8dfaa9
Johan Lindberg <johan@pulp.se>
2013-04-20 10:15:18 +0200

    Added an empty pyglet application.

---
+++ README.md
@@ -0,0 +1,3 @@
+## The Moonville Saga
+
+The Moonville Saga is a collection of short stories and games about our efforts to colonize the moon.
---
+++ moonville.py
@@ -0,0 +1,11 @@
+## Moonville
+
+import pyglet
+import pyglet.window
+
+class Moonville(pyglet.window.Window):
+    pass
+
+if __name__ == "__main__":
+    moonville = Moonville()
+    pyglet.app.run()
