9b48f423d707f590c146c3801aa40c190dbc331f
Johan Lindberg <johan@pulp.se>
2013-04-20 10:35:49 +0200

    Added model.py containing a number of classes for game objects.

---
+++ model.py
@@ -0,0 +1,16 @@
+## Moonville
+
+
+## Objects
+class Robonaut(object):
+    pass
+
+class Solar_Cell(object):
+    pass
+
+class He3_Processor(object):
+    pass
+
+class Digger(object):
+    pass
+
