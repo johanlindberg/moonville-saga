38f495ec5852f532b6006973147a804ab5a51c7b
Johan Lindberg <johan@pulp.se>
2013-04-20 12:43:50 +0200

    Added a background image.

--- moonville.py
+++ moonville.py
@@ -3,9 +3,25 @@
 import pyglet
 import pyglet.window
 
+import model
+
 class Moonville(pyglet.window.Window):
     def __init__(self):
         super(Moonville, self).__init__(width = 800, height = 600)
+
+        self.__state__ = self.introduction
+        self.load_images()
+
+    def load_images(self):
+        self.background_image = pyglet.image.load("background.png")
+
+    def on_draw(self):
+        self.__state__()
+            
+    # Scenes
+    def introduction(self):
+        self.clear()
+        self.background_image.blit(0,0)
 
     # Event handlers
     def on_key_press(self, symbol, modifiers):
