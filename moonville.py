1b07f789b290e57307cc19efc54f51aaf7ac83d4
Johan Lindberg <johan@pulp.se>
2013-04-20 10:22:02 +0200

    Added a handler for key presses.

--- moonville.py
+++ moonville.py
@@ -4,7 +4,13 @@ import pyglet
 import pyglet.window
 
 class Moonville(pyglet.window.Window):
-    pass
+    def __init__(self):
+        super(Moonville, self).__init__(width = 800, height = 600)
+
+    # Event handlers
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.Q:
+            pyglet.app.exit()
 
 if __name__ == "__main__":
     moonville = Moonville()
