3b94f347aaba02b651cc1c3d44f35f68e22ed8d4
Johan Lindberg <johan@pulp.se>
2013-04-20 13:24:34 +0200

    Refactoring how to handle scenes.

--- moonville.py
+++ moonville.py
@@ -9,24 +9,29 @@ class Moonville(pyglet.window.Window):
     def __init__(self):
         super(Moonville, self).__init__(width = 800, height = 600)
 
-        self.__state__ = self.introduction
+        self.__state__ = Introduction()
         self.load_images()
 
     def load_images(self):
         self.background_image = pyglet.image.load("background.png")
 
-    def on_draw(self):
-        self.__state__()
-            
-    # Scenes
-    def introduction(self):
-        self.clear()
-        self.background_image.blit(0,0)
-
     # Event handlers
+    def on_draw(self):
+        self.__state__.on_draw(self)
+            
     def on_key_press(self, symbol, modifiers):
+        self.__state__.on_key_press(self, symbol, modifiers)
+            
+## Scenes
+class Introduction(object):
+    def on_draw(self, window):
+        window.clear()
+        window.background_image.blit(0,0)
+
+    def on_key_press(self, window, symbol, modifiers):
         if symbol == pyglet.window.key.Q:
             pyglet.app.exit()
+
 
 if __name__ == "__main__":
     moonville = Moonville()
